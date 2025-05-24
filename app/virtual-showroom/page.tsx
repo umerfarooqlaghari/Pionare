0286d16fb255f8d7268ec55d78aa4c68c4ca60a6
umerfarooqlaghari <mumerfarooqlaghari@gmail.com>
2025-05-25 02:48:55 +0500

    commented virtual showroom

--- app/virtual-showroom/page.tsx
+++ app/virtual-showroom/page.tsx
@@ -1,10 +1,10 @@
-import OfficeShowroom from '../Components/OfficeShowroomComponent/OfficeShowroom';
-import Tour360Viewer from '../Components/Tour360Component/Tour360Viewer';
-export default function VirtualShowroom() {
-  return (
-    <>
-      {/* <Tour360Viewer /> */}
-      <OfficeShowroom/>
-    </>
-  );
-}
+// import OfficeShowroom from '../Components/OfficeShowroomComponent/OfficeShowroom';
+// import Tour360Viewer from '../Components/Tour360Component/Tour360Viewer';
+// export default function VirtualShowroom() {
+//   return (
+//     <>
+//       {/* <Tour360Viewer /> */}
+//       <OfficeShowroom/>
+//     </>
+//   );
+// }
